8d644e3d38e006baa7c58f75686098cedd101a69
Andrew Fitz Gibbon <fitz@temporal.io>
2023-08-09 13:23:56 -0500

    parallel exec load

--- scripts/simulate_load.sh
+++ scripts/simulate_load.sh
@@ -3,25 +3,45 @@
 SLEEP_INTERVAL_MAX=5
 POINTS_MAX=100
 INVITE_CHANCE=0.01
+MAX_CUSTOMERS_PER_CYCLE=10
 
-while true; do
+DATE_STAMP="date -Iseconds"
+
+function signalCustomer() {
     workflow=$(temporal workflow list -o json --query "ExecutionStatus = 'Running'" | \
         jq '.[].execution.workflow_id' -r | sort -R | tail -n 1)
-    echo "Chose customer workflow ID: $workflow"
-    
-    points=$(( $RANDOM % $POINTS_MAX + 1))
-    echo -e "\t--> Signaling to add $points points"
-    echo -n -e "\t--> "
-    temporal workflow signal -w $workflow --name addLoyaltyPoints -i $points
 
-    if [[ $(( $RANDOM % 100 )) < $(( $INVITE_CHANCE * 100 )) ]]; then
-        guestId=$RANDOM
-        echo -e "\t--> Signaling to invite guest with ID $guestId"
-        echo -n -e "\t--> "
-        signal_cmd="temporal workflow signal -w $workflow --name inviteGuest -i $(echo -n "'\"${guestId}\"'")"
-        eval $signal_cmd
+    random=$(od -vAn -N2 -tu2 < /dev/urandom | awk '{print $1}')
+    points=$(( $random % $POINTS_MAX + 1 ))
+
+    echo -e "[$(eval $DATE_STAMP) $workflow] Signaling to add $points points"
+    temporal workflow signal -w $workflow --name addLoyaltyPoints -i $points &> /dev/null
+    if [ $? -ne 0 ]; then
+        echo -e "[$workflow] failed to signal add points"
     fi
 
-    echo -e "\t--> End of cycle; sleeping"
-    sleep $(( $RANDOM % $SLEEP_INTERVAL_MAX + 1 ))
+    random=$(od -vAn -N2 -tu2 < /dev/urandom | awk '{print $1}')
+    if [[ $(( $random % 100 )) < $(( $INVITE_CHANCE * 100 )) ]]; then
+        guestId=$random # no need to regenerate random for the ID
+
+        echo -e "[$(eval $DATE_STAMP) $workflow] Signaling to invite guest with ID $guestId"
+        signal_cmd="temporal workflow signal -w $workflow --name inviteGuest -i $(echo -n "'\"${guestId}\"'")"
+        eval $signal_cmd &> /dev/null
+        if [ $? -ne 0 ]; then
+            echo -e "[$(eval $DATE_STAMP) $workflow] failed to signal invite guest"
+        fi
+    fi
+}
+
+while true; do
+    numCustomers=$(( $RANDOM % $MAX_CUSTOMERS_PER_CYCLE + 1 ))
+    for i in `seq 1 $numCustomers`; do
+        # May select the same customer multiple times, and that's okay.
+        signalCustomer &
+    done
+
+    sleepTime=$(( $RANDOM % $SLEEP_INTERVAL_MAX + 1 ))
+    wait
+    echo -e "[$(eval $DATE_STAMP)] End of cycle; sleeping for ${sleepTime}s"
+    sleep $sleepTime
 done
